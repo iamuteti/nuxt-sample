6e75c8501c57171ff87cbdf1c3fb66a5c1dd0891
iamuteti <iamuteti@gmail.com>
2023-04-19 10:50:38 +0300

    test page

---
+++ pages/test.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <Head>
+        <Title>TEST ROASTING</Title>
+      </Head>
+  
+      <div>
+        <h1>TEST TRACKING URL PAGE</h1>
+      </div>
+    </div>
+  </template>
